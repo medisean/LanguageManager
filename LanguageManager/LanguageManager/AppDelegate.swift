a26ba9116e5c5d551ee9f0ecd680b3acf676d99b
liangmingzou@163.com <liangmingzou@163.com>
2016-05-30 22:33:08 +0800

    Add project

---
+++ LanguageManager/LanguageManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LanguageManager
+//
+//  Created by bruce on 16/5/30.
+//  Copyright © 2016年 bruce. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LanguageManager/LanguageManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LanguageManager
+//
+//  Created by bruce on 16/5/30.
+//  Copyright © 2016年 bruce. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
